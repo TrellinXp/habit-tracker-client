fe6ec32bb83004f3f9c43b2f4b0050006c34e342
Peter Mayer <peter_my@gmx.de>
2021-08-26 14:40:20 +0200

    Is Good Habit Amount number

--- src/components/CreateHabit.js
+++ src/components/CreateHabit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
  
 export default class CreateHabit extends Component {
-    state = { title: "", description: "", amount:"", unit:"", isShowing: false } 
+    state = { title: "", description: "", amount:1, unit:"", isShowing: false } 
 
     handleFormSubmit = (event) => {
         event.preventDefault();
@@ -11,6 +11,11 @@ export default class CreateHabit extends Component {
         const description = this.state.description;
         const amount = this.state.amount;
         const unit = this.state.unit;
+
+        let goodHabit = false;
+        if(this.state.goodHabit == 'on') {
+           goodHabit = true;
+        }
         const date = Date.now();
 
         const user = this.props.user._id; // <== we need to know to which project the created task belong, so we need to get its 'id'
@@ -19,7 +24,7 @@ export default class CreateHabit extends Component {
         
         // { title, description, projectID } => this is 'req.body' that will be received on the server side in this route, 
         // so the names have to match
-        axios.post("http://localhost:5000/api/habits/",  { title, description, amount, unit, date, user } )
+        axios.post("http://localhost:5000/api/habits/",  { title, description, amount, unit, date, user, goodHabit } )
         .then( () => {
             // after submitting the form, retrieve project one more time so the new task is displayed as well 
             //this.setState({title: "", description: "", amount:"", unit:""});
@@ -43,10 +48,11 @@ export default class CreateHabit extends Component {
                     <label>Description:</label>
                     <textarea name="description" value={this.state.description} onChange={ e => this.handleChange(e)} />
                     <label>Amount:</label>
-                    <textarea name="amount" value={this.state.amount} onChange={ e => this.handleChange(e)} />
+                    <input name="amount" type="number" value={this.state.amount} onChange={ e => this.handleChange(e)} />
                     <label>Unit:</label>
                     <textarea name="unit" value={this.state.unit} onChange={ e => this.handleChange(e)} />
-                    
+                    <label>Good Habit:</label>
+                    <input type="checkbox" id="goodHabit" name="goodHabit" onChange={ e => this.handleChange(e)} />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
