22010eaf964fc4e9e501c7b38353f26071f59180
TrellinXp <Peter_My@gmx.de>
2021-09-01 16:23:33 +0200

    Is Good Habit

--- src/components/calendar/HabitTile.js
+++ src/components/calendar/HabitTile.js
@@ -14,6 +14,13 @@ export default class HabitTile extends Component {
             habits: filteredHabits
         });
     }
+
+    getIsGoodHabit(habit) {
+        if(habit.goodHabit) {
+            return "*Good*";
+        } 
+        return "*Bad*";
+    }
     
     render() {
         return (
@@ -21,11 +28,11 @@ export default class HabitTile extends Component {
                 {this.props.listOfHabitsObj.slice(0, 10).map(habit => {
                     if (habit.goodHabit) {
                         return (<div key={habit._id} className="habit-tile-good">
-                            <Link className="detail-button" to={`/habits/${habit._id}`}><h4 className="habit-title">{habit.title}</h4></Link>
+                            <Link className="detail-button" to={`/habits/${habit._id}`}><h4 className="habit-title"><b>{this.getIsGoodHabit(habit)}</b>{habit.title}</h4></Link>
                         </div>);
                     } else {
                         return (<div key={habit._id} className="habit-tile-bad">
-                            <Link className="detail-button" to={`/habits/${habit._id}`}><h4 lassName="habit-title">{habit.title}</h4></Link>
+                            <Link className="detail-button" to={`/habits/${habit._id}`}><h4 lassName="habit-title"><b>{this.getIsGoodHabit(habit)}</b>{habit.title}</h4></Link>
                         </div>);
                     }
                 })
